2384e7bd894e33093a6d06eb8052ca287abc84f5
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-09-26 22:06:55 +0200

    feat : Iteracion #4 : Arrays neoland-basic-algorithms

--- Javascript/neoland-basic-algorithms/app.js
+++ Javascript/neoland-basic-algorithms/app.js
@@ -40,24 +40,66 @@ console.log(car2.basePrice);
 
 //Iteracion #3 : Operadores
 //1.1
+
 console.log(10*5);
+
 //1.2
+
 let division = 10/2;
 console.log(division);
+
 //1.3
+
 let modulo = 15%9;
 console.log(modulo);
+
 //1.4
+
 let o = 15;
 let p = 10;
 let j = 5;
 
 o = p + j;
+
 //1.5
+
 let i = 50;
 let c = 10;
 let m = 5;
 
 i = c * m;
+
+//Iteracion #4 : Arrays
+
+//1.1
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+//1.2
+
+avengers[0] = "IRON MAN";
+
+//1.3
+
+console.log(avengers.length);
+
+//1.4
+
+//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+//rickAndMortyCharacters.push("Morty","Summer");
+//console.log(rickAndMortyCharacters.length-1);
+
+//1.5
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters[0] + "\r\n" + rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
+
+//1.6
+
+rickAndMortyCharacters.splice(2,1);
+rickAndMortyCharacters.push("Lapiz Lopez");
+console.log(rickAndMortyCharacters);
 
 
