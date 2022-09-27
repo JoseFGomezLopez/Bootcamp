5dd1dc69b91b35f44b93806313d4bb793d48468a
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-09-27 20:53:00 +0200

    feat : Iteracione 5 : calcular promedio de string : neoland-basic-functions

--- Javascript/neoland-basic-functions/app.js
+++ Javascript/neoland-basic-functions/app.js
@@ -2,10 +2,10 @@
 
 //Ejercicio 1.1
 
-function sum(numberOne , numberTwo) {
-    return (numberOne > numberTwo) ? numberOne : numberTwo;
-  }
-  
+function sum(numberOne, numberTwo) {
+  return numberOne > numberTwo ? numberOne : numberTwo;
+}
+
 //Ejercicio 1.2
 /*
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
@@ -21,18 +21,17 @@ return console.log(theLargestWord);
 }
 findLongestWord(avengers);
 */
-//Iteracion 3 
+//Iteracion 3
 
 const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumAll(arr) {
-    
-  let sumatorio = 0; 
-  
+  let sumatorio = 0;
+
   for (let i = 0; i < arr.length; i++) {
-          sumatorio += arr[i];  
-    }
-    return sumatorio;
+    sumatorio += arr[i];
+  }
+  return sumatorio;
 }
 sumAll(numbers);
 
@@ -43,9 +42,27 @@ function average(param) {
   let sumatorio = 0;
   let promedio = 0;
   for (let i = 0; i < param.length; i++) {
-     sumatorio += param[i];    
-    }
-    promedio = sumatorio/param.length;
-    return promedio;
+    sumatorio += param[i];
+  }
+  promedio = sumatorio / param.length;
+  return promedio;
 }
 average(numbers2);
+
+//Iteracion 5 : Calcular promedio de Strings
+
+const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+function averageWord(param) {
+  let sumatorio = 0;
+  for (let i = 0; i < param.length; i++) {
+    if (typeof param[i] == "string") {
+      sumatorio += param[i].length;
+    } else if(typeof param[i] == "number"){
+      sumatorio += param[i];
+    } else{ 
+      continue;
+    }
+  }
+  return sumatorio;
+}
+averageWord(mixedElements);
