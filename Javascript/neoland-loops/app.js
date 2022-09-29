29742f14dc12af4f01d77d69fec91c22a401f4c7
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-09-29 17:20:55 +0200

    feat : Iteracion 4 LOOPS, neoland-loops

--- Javascript/neoland-loops/app.js
+++ Javascript/neoland-loops/app.js
@@ -77,3 +77,20 @@ const placesToTravel = [
 for (const place of placesToTravel) {
   console.log(place);
 }
+
+//Iteración #4: Probando For...in
+/*Usa un for...in para imprimir por 
+consola los datos del alienígena.. Puedes usar este objeto: */
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+};
+
+for (const key in alien) {
+        console.log(`${key} : ${alien[key]}`)
+    }
+
+
