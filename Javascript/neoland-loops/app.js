aa610215964da7f7c9f15673d69980eabb221a71
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-09-29 16:58:51 +0200

    feat : Iteracion 2 LOOPS, neoland-loops

--- Javascript/neoland-loops/app.js
+++ Javascript/neoland-loops/app.js
@@ -1,18 +1,57 @@
-
-//Iteracion 1: 
+//Iteracion 1:
 
 const products = [
-    "Camiseta de Pokemon",
-    "Pantalón coquinero",
-    "Gorra de gansta",
-    "Camiseta de Basket",
-    "Cinrurón de Orión",
-    "AC/DC Camiseta",
-  ];
-  
-  for (let i = 0; i < products.length; i++) {
-      if(products[i].includes("Camiseta")){
-          console.log(products[i]);
-      }
-      
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].includes("Camiseta")) {
+    console.log(products[i]);
   }
+}
+
+//Iteración #2: Condicionales avanzados
+
+/* 
+  Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y 
+  añade la propiedad isApproved a true o false en consecuencia.
+  Una vez lo tengas compruébalo con un console.log. 
+  */
+
+const alumns = [
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
+const approvedSearcher = (initialArray) => {
+  for (let i = 0; i < initialArray.length; i++) {
+    if (
+      initialArray[i].T1 == true &&
+      (initialArray[i].T2 == true || initialArray[i].T3 == true)
+    ) {
+      initialArray[i].isApproved = true;
+      console.log(`${initialArray[i].name} : ${initialArray[i].isApproved}`);
+    } else if (
+      initialArray[i].T3 == true &&
+      (initialArray[i].T2 == true || initialArray[i].T1 == true)
+    ) {
+      initialArray[i].isApproved = true;
+      console.log(`${initialArray[i].name} : ${initialArray[i].isApproved}`);
+    } else if (
+      initialArray[i].T2 == true &&
+      (initialArray[i].T3 == true || initialArray[i].T1 == true)
+    ) {
+      initialArray[i].isApproved = true;
+      console.log(`${initialArray[i].name} : ${initialArray[i].isApproved}`);
+    }
+  }
+};
+
+approvedSearcher(alumns);
