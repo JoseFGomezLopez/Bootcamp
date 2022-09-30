6005eaf45ac0183e662d9de9bdf4810091c968cc
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-09-30 20:44:11 +0200

    feat : Iteracion #2 JS Review Basics

--- Javascript/Exercices/JS Review Basics/app.js
+++ Javascript/Exercices/JS Review Basics/app.js
@@ -30,3 +30,68 @@ for (const movie of movies) {
   }
 }
 console.log(moviesCategories);
+
+//Iteracion #2 : Mix Fors
+/*Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos 
+los sonidos favoritos que tienen los usuarios.*/
+
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const arrayFavoritesSounds = [];
+const sounds = [];
+const volume = [];
+counter = 0;
+
+for (const user of users) {
+  for (const key in user) {
+    //console.log(user.favoritesSounds);
+    arrayFavoritesSounds.unshift(user.favoritesSounds);
+  }
+}
+
+for (const sound of arrayFavoritesSounds) {
+  for (const typeOfSound in sound) {
+    sounds.unshift(Object.values(sound));
+  }
+}
+
+for (const type of sounds) {
+  for (const feature of type) {
+    counter += feature.volume;
+  }
+}
+
+console.log(counter);
