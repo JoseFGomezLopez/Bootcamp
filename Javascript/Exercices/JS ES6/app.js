198064b32637284bb5d31ddd407ff80418d3ccd9
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-10-01 20:32:55 +0200

    feat : Iteracion #1 JS ES6

---
+++ Javascript/Exercices/JS ES6/app.js
@@ -0,0 +1,15 @@
+
+//Iteracion #1 : 
+//Crea una arrow function que tenga dos parametros a y b y 
+//que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+//por consola la suma de los dos parametros.
+//1.1 : 1.1 Ejecuta esta función sin pasar ningún parametro
+//1.2 Ejecuta esta función pasando un solo parametro
+//1.3 Ejecuta esta función pasando dos parametros
+
+var sum = (a = 10,b = 5) =>  a + b;
+  
+console.log(sum());
+console.log(sum(12));
+console.log(sum(23,32));
+
