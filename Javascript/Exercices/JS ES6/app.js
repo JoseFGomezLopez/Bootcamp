778a5d4d6cc79e0d818795e77069821eab7935f5
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-10-02 11:06:42 +0200

    feat : Iteracion #3 JS ES6

--- Javascript/Exercices/JS ES6/app.js
+++ Javascript/Exercices/JS ES6/app.js
@@ -48,7 +48,7 @@ const animalFunction = () => {
   return { name: "Bengal Tiger", race: "Tiger" };
 };
 
-let {param1,param2} = animalFunction();
+let { param1, param2 } = animalFunction();
 
 console.log(param1);
 console.log(param2);
@@ -60,14 +60,57 @@ console.log(param2);
 
 const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
 
-let {name,itv} = car;
+let { name, itv } = car;
 
 console.log(name);
 console.log(itv);
 
-let[año1,año2,año3] = itv;
+let [año1, año2, año3] = itv;
 
 console.log(año1);
 console.log(año2);
 console.log(año3);
+
+// Iteracion #3 : Spread Operator
+
+//3.1 Dado el siguiente array, crea una copia usando spread operators.
+
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+const newArray = [...pointsList];
+console.log(newArray);
+
+//3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+
+const toyCopied = { ...toy };
+console.log(toyCopied);
+
+//3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
+//spread operatos.
+
+const pointsList2 = [32, 54, 21, 64, 75, 43];
+const pointsLis3 = [54, 87, 99, 65, 32];
+
+const newArray2 = [...pointsList2, ...pointsLis3];
+console.log(newArray2);
+
+//3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
+//con spread operators.
+
+const toy2 = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const toyUpdate = { lights: "rgb", power: ["Volar like a dragon", "MoonWalk"] };
+
+const newToyObject = { ...toy2, ...toyUpdate };
+console.log(newToyObject);
+
+//3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
+//pero sin editar el array inicial. De nuevo, usando spread operatos.
+
+const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
+
+const newArrayColors = [...colors.slice(0,2), ...colors.slice(3,colors.length)];
+console.log(newArrayColors);
+
 
