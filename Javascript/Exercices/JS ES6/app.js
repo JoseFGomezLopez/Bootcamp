8ef78c62667a033ee8be93465aabc33cdd88738d
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-10-02 13:11:44 +0200

    feat : Iteracion #4 : Map JS ES6

--- Javascript/Exercices/JS ES6/app.js
+++ Javascript/Exercices/JS ES6/app.js
@@ -110,7 +110,59 @@ console.log(newToyObject);
 
 const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
 
-const newArrayColors = [...colors.slice(0,2), ...colors.slice(3,colors.length)];
+const newArrayColors = [
+  ...colors.slice(0, 2),
+  ...colors.slice(3, colors.length),
+];
 console.log(newArrayColors);
 
+//Iteracion #4 : Map
 
+// 4.1 Dado el siguiente array, devuelve un array con sus nombres
+// utilizando .map().
+
+const users = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+
+const usersNames = users.map((userName) => userName.name);
+console.log(usersNames);
+
+// 4.2 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que
+// empiece por 'A'.
+
+const users2 = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+
+const usersNames2 = users2.map((userName) => {
+  if (userName.name.startsWith("A")) {
+    return (userName.name = "Anacleto");
+  } else {
+    return userName.name;
+  }
+});
+console.log(usersNames2);
+
+// 4.3 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y añade al valor de .name el string ' (Visitado)'
+// cuando el valor de la propiedad isVisited = true.
+
+const cities = [
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: false, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
+
+const citiesVisited = cities
+  .filter((city) => city.isVisited == true)
+  .map((city) => (city.name += " Visitado"));
+console.log(citiesVisited);
