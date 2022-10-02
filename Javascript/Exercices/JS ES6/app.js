f06ffdd0c029e4c1662b2ba24596971af9603942
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-10-02 18:40:20 +0200

    feat : Iteracion #6 Find JS ES6

--- Javascript/Exercices/JS ES6/app.js
+++ Javascript/Exercices/JS ES6/app.js
@@ -231,11 +231,11 @@ console.log(legendsStreames);
 // const input = document.querySelector("input");
 
 // function buscarNombre(e) {
-  
+
 //   const result = streamers.filter((streamer) => {
 //     if (streamer.name.includes(input.value)) {
 //        console.log(streamer.name);
-//       return input.textContent= streamer.name; 
+//       return input.textContent= streamer.name;
 //     }
 //   });
 // }
@@ -247,15 +247,59 @@ console.log(legendsStreames);
 // me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
 // En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
 
-const input5_7 = document.querySelector("input");
-const btn = document.querySelector("button");
-  
-  const e = () => {
-    const result2 = streamers.filter((streamer) => {
-      if (streamer.name.includes(input5_7.value)) {
-        console.log(streamer.name);
-        return streamer.name;
-      }
-    });
-  };
-  btn.addEventListener("click",e); 
+// const input5_7 = document.querySelector("input");
+// const btn = document.querySelector("button");
+
+// const e = () => {
+//   const result2 = streamers.filter((streamer) => {
+//     if (streamer.name.includes(input5_7.value)) {
+//       console.log(streamer.name);
+//       return streamer.name;
+//     }
+//   });
+// };
+// btn.addEventListener("click", e);
+
+//Iteracion #6 : Find
+//6.1 Dado el siguiente array, usa .find() para econtrar el número 100.
+
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+//6.2 Dado el siguiente array, usa .find() para econtrar la pelicula del año 2010.
+
+const movies = [
+  { title: "Madagascar", stars: 4.5, date: 2015 },
+  { title: "Origen", stars: 5, date: 2010 },
+  { title: "Your Name", stars: 5, date: 2016 },
+];
+
+const movie2010 = movies.find(movie => movie.date = 2010);
+console.log(movie2010);
+
+//6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre
+//'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa
+//spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación
+//lo queremos meter en la propiedad .mutation del objeto fusionado.
+
+const aliens = [
+  { name: "Zalamero", planet: "Eden", age: 4029 },
+  { name: "Paktu", planet: "Andromeda", age: 32 },
+  { name: "Cucushumushu", planet: "Marte", age: 503021 },
+];
+const mutations = [
+  {
+    name: "Porompompero",
+    description:
+      "Hace que el alien pueda adquirir la habilidad de tocar el tambor",
+  },
+  {
+    name: "Fly me to the moon",
+    description: "Permite volar, solo y exclusivamente a la luna",
+  },
+  {
+    name: "Andando que es gerundio",
+    description: "Invoca a un señor mayor como Personal Trainer",
+  },
+];
+const mutant = {...aliens.find(alien => alien.name = "Cucushumushu"),...mutations.find(mutation => mutation.name = "Porompompero")};
+console.log(mutant); 
