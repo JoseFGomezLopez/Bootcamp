0ebead2b9d16c78116f9251be41fdd377fa6c842
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-10-03 16:27:31 +0200

    feat : Iteracion #7 Reduce JS ES6

--- Javascript/Exercices/JS ES6/app.js
+++ Javascript/Exercices/JS ES6/app.js
@@ -301,5 +301,40 @@ const mutations = [
     description: "Invoca a un señor mayor como Personal Trainer",
   },
 ];
-const mutant = {...aliens.find(alien => alien.name = "Cucushumushu"),...mutations.find(mutation => mutation.name = "Porompompero")};
+const mutant = {...aliens.find(alien => alien.name == "Cucushumushu"),...mutations.find(mutation => mutation.name == "Porompompero")};
 console.log(mutant); 
+
+//Iteracion #7: 
+
+//7.1 Dado el siguiente array, haz una suma de todos las notas de 
+//los examenes de los alumnos usando la función .reduce().
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+const sumAllScores = exams.reduce((acc,score) => acc + score.score,0);
+console.log(sumAllScores);
+
+//7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+//alumnos que esten aprobados usando la función .reduce().
+
+const sumAllScoresApproved = exams
+.filter(score => score.score>=5)
+.reduce((acc,score) => acc + score.score,0);
+console.log(sumAllScoresApproved);
+
+//7.3 Dado el mismo array, haz la media de las notas de todos los 
+//examenes .reduce().
+
+const media = exams.reduce((acc,score) => acc + score.score,0)/exams.length;
+console.log(media);
