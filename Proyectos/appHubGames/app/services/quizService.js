b87a2af5e75cde4583849b84e9b95c01d63669c3
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-10-23 12:53:17 +0200

    feat : modicacion de la url del server local

--- Proyectos/appHubGames/app/services/quizService.js
+++ Proyectos/appHubGames/app/services/quizService.js
@@ -3,7 +3,7 @@ import { errorComponent } from '../../app/components/Error/error'
 
 export const quizApi = async() => {
     try{
-    const res = await fetch('http://localhost:3000/quiz');
+    const res = await fetch('https://serverappgames.onrender.com/quiz');
     const data = await res.json();
     return data
 }catch(error){
