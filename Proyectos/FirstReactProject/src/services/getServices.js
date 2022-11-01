1a159fa7fc7cc0f3459b1f6961f6182d2d7fcfbc
JoseGomezLopez <fontanadeoro1983@gmail.com>
2022-11-01 22:24:45 +0100

    feat : dist

--- Proyectos/FirstReactProject/src/services/getServices.js
+++
@@ -1,7 +0,0 @@
-
-export const getApis = async() => {
-     const response = await fetch('https://api.fbi.gov/wanted/v1/list');
-     const data = await response.json();
-     console.log(data);
-     return data;
-}
